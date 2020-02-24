a9b84cff7174de19896f46787def7f942ef5de84
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2020-02-24 16:34:45 +0530

    3 wavelengths

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 current_color = 0;
 scale_value = 1.5;
-
+color_string = ["570 nm", "580 nm", "590 nm", "Black & White"]
 function initializer(){
   autoSize();
   randomChar();
@@ -31,13 +31,26 @@ function getDPI(){
 
 function setColor(option){
   if(option == 0){
-    document.getElementById("body_main").style.background = "rgb(255, 255, 0)";
+    // 570 nm
+    document.getElementById("body_main").style.background = "rgb(225, 255, 0)";
     document.getElementById("body_main").style.color = "rgb(0, 0, 255)";
   }
   else if(option == 1){
+    // 580
+    document.getElementById("body_main").style.background = "rgb(255, 255, 0)";
+    document.getElementById("body_main").style.color = "rgb(0, 0, 255)";
+  }
+  else if(option == 2){
+    // 590
+    document.getElementById("body_main").style.background = "rgb(255, 223, 0)";
+    document.getElementById("body_main").style.color = "rgb(0, 0, 255)";
+  }
+  else if(option == 3){
+    // Black and white
     document.getElementById("body_main").style.background = "rgb(255, 255, 255)";
     document.getElementById("body_main").style.color = "rgb(0, 0, 0)";
   }
+
 }
 
 function setSize(inp){
@@ -57,13 +70,17 @@ var current_value = 1.0;
 var max_value = 1.0;
 var min_value = -0.3;
 
+function display(){
+  str = (current_value).toFixed(1);
+  document.getElementById("info").innerHTML ="<br>" + str + " logMAR | "  + "Color:" + color_string[current_color] ;
+}
+
 
 function autoSize(){
   var distance = 4.0 * Math.pow(10, current_value);
   var h = Math.tan(5/60 * Math.PI/180) * distance * 100;
   console.log("SIZE" + distance)
-  str = (current_value).toFixed(1);
-  document.getElementById("info").innerHTML ="<br>" + str + " logMAR";
+  display()
   setSize(h);
 
 }
@@ -91,9 +108,10 @@ function randomChar(){
 }
 
 function changeColor(){
-  if(current_color == 1) current_color = 0;
-  else current_color = 1;
+  current_color = current_color + 1;
+  if(current_color > 3)current_color = 0;
   setColor(current_color)
+
 }
 
 document.onkeydown = function(event) {
